705b537540f8df418dad1a875c05acb6a4d213c5
Alex Wilson <a@ax.gy>
2018-08-22 22:29:00 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dummy Service
+
+A simple, minimal express service for use in Fastly as a safe, known backend.
---
+++ main.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.disable('etag')
+app.disable('x-powered-by')
+
+app.use(cookieParser())
+app.use('/__gtg', (req, res) => 
+  res
+    .header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+    .send('OK')
+)
+app.use((req, res) =>
+  res
+    .header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+    .json({
+      cookies: req.cookies,
+      headers: req.headers,
+      hostname: req.hostname,
+      method: req.method,
+      url: req.url
+    })
+)
+
+const listener = app.listen(process.env.PORT || 3000, err => {
+    if (err) throw err
+    const {address,port} = listener.address()
+    console.info(`Serving on ${address} ${port}.`)
+})
