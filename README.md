b6fbe306dfeb07e9df94957f8e27fae62f20b58a
Alex Wilson <a@ax.gy>
2019-01-06 12:50:22 +0000

    document, and add response and delay overrides

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 A simple, minimal express service for use in Fastly for testing and debugging, as a safe, known backend.
 It will return all HTTP data sent to it.
+
+There is a public instance running on `dummy.now.sh`.
+
+---
+
+*Warning*: This will reflect _everything_ that is sent to it, including potentially your authentication/authorization secrets.
+Please take care not to leak anything sensitive.
+
+---
+
+With Dummy Service there are a couple of special-case headers which alter the behaviour of the service.
+
+- `Dummy-Status`: This overrides the status-code returned by a given call. Defaults to 200. (e.g. return a 403)
+- `Dummy-Response`: This overrides the response body returned by a given call. Otherwise will return all headers and cookies in a JSON blob. (e.g. return an error page)
+- `Dummy-Delay`: This adds a delay (in microseconds) in the processing of the response, useful for testing a stalled origin. Defaults to off.
+- `Dummy-Cache-Control`: This overrides the cache-control header returned. Defaults to `private` to prevent caching.
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const express = require('express')
 const cookieParser = require('cookie-parser')
 
@@ -11,18 +13,33 @@ app.use('/__gtg', (req, res) =>
     .header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
     .send('OK')
 )
-app.use((req, res) =>
-  res
-    .header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
-    .status(req.headers['Dummy-Status'] || 200)
-    .json({
-      cookies: req.cookies,
-      headers: req.headers,
-      hostname: req.hostname,
-      method: req.method,
-      url: req.url
-    })
-)
+app.use((req, res) => {
+  // Simulate a stalled origin.
+  if (req.get('Dummy-Delay')) {
+    const send = res.send;
+    res.send = (...args) => setTimeout(_ => {
+            send.apply(res, args)
+    }, req.get('Dummy-Delay'))
+  }
+
+  // Initialise the response.
+  const response = res
+    .header('Cache-Control', req.header('Dummy-Cache-Control') || 'private, no-cache, no-store, must-revalidate')
+    .status(req.header('Dummy-Status') || 200)
+
+  // Override the response if a body has been specified.
+  return (req.header('Dummy-Response')) ?
+    response
+      .send(req.header('Dummy-Response')) :
+    response
+      .json({
+        cookies: req.cookies,
+        headers: req.headers,
+        hostname: req.hostname,
+        method: req.method,
+        url: req.url
+      })
+})
 
 const listener = app.listen(process.env.PORT || 3000, err => {
     if (err) throw err
